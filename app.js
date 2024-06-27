bc74e8a50df26513eb5b3bf8e6a2ff2fc4a95dfb
itsFarhanPatel <113057507+itsFarhanPatel@users.noreply.github.com>
2024-06-27 19:24:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.send('Hello, DEVOPS, This is Updated code');
 });
 
 app.listen(3000, () => {
