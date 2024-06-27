cdbe6756192309866e259191666f5eee6d0860bd
root <root@ip-172-31-10-9.us-west-1.compute.internal>
2024-06-27 14:02:20 +0000

    Updated code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.send('Hello, DevOps. This is Updated code');
 });
 
 app.listen(3000, () => {
